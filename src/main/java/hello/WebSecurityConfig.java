cc6657297cba2664661b74104d0f5ae5d66d0eec
TEBDEX <tebdex@tebdex.com>
2024-07-07 08:56:29 +0000

    Apply tebdex transformation

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,28 +1,27 @@
 package hello;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebMvcSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
-                    .anyRequest().authenticated();
+                    .authorizeRequests(withDefaults());
         http
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+                    .formLogin(login -> login
+                                .logout(logout -> logout
+                                            .permitAll()));
+        return http.build();
     }
 
     @Configuration
--- src/main/java/org/thymeleaf/spring/support/ThymeleafLayoutInterceptor.java
+++ src/main/java/org/thymeleaf/spring/support/ThymeleafLayoutInterceptor.java
@@ -2,17 +2,17 @@ package org.thymeleaf.spring.support;
 
 import org.springframework.util.Assert;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 /**
  * From https://github.com/kolorobot/thymeleaf-custom-layout
  */
-public class ThymeleafLayoutInterceptor extends HandlerInterceptorAdapter {
+public class ThymeleafLayoutInterceptor implements HandlerInterceptor {
 
     private static final String DEFAULT_LAYOUT = "layouts/default";
     private static final String DEFAULT_VIEW_ATTRIBUTE_NAME = "view";
@@ -21,12 +21,12 @@ public class ThymeleafLayoutInterceptor extends HandlerInterceptorAdapter {
     private String viewAttributeName = DEFAULT_VIEW_ATTRIBUTE_NAME;
 
     public void setDefaultLayout(String defaultLayout) {
-        Assert.hasLength(defaultLayout);
+        Assert.hasLength(defaultLayout, "must have length; it must not be null or empty");
         this.defaultLayout = defaultLayout;
     }
 
     public void setViewAttributeName(String viewAttributeName) {
-        Assert.hasLength(defaultLayout);
+        Assert.hasLength(defaultLayout, "must have length; it must not be null or empty");
         this.viewAttributeName = viewAttributeName;
     }
 
